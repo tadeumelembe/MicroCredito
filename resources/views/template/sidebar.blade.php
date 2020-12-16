ca20c9604506adda4a996d56200a20fe190b855e
tadeu_melembe <tadeumelembe@gmail.com>
2020-12-16 14:24:05 +0200

    side bar

---
+++ resources/views/template/sidebar.blade.php
@@ -0,0 +1,40 @@
+<div data-simplebar class="h-100">
+
+    <!--- Sidemenu -->
+    <div id="sidebar-menu">
+        <!-- Left Menu Start -->
+        <ul class="metismenu list-unstyled" id="side-menu">
+            <li class="menu-title">Menu</li>
+
+            <li>
+                <a href="/home" class="waves-effect">
+                    <i class="ri-dashboard-line"></i><span class="badge badge-pill badge-success float-right">3</span>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="/clientes" class=" waves-effect">
+                    <i class=" ri-team-fill"></i>
+                    <span>Clientes</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="/emprestimos" class=" waves-effect">
+                    <i class=" ri-hand-coin-fill"></i>
+                    <span>Empréstimos</span>
+                </a>
+            </li>
+
+            <li>
+                <a href="/users" class=" waves-effect">
+                    <i class="ri-group-fill"></i>
+                    <span>Usuário</span>
+                </a>
+            </li>
+
+        </ul>
+    </div>
+    <!-- Sidebar -->
+</div>
