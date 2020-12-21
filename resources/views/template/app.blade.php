e520c0360f39b8a4bdfbaf6457fe7442e9501447
Elton Júlio <elton.cuambe@uem.ac.mz>
2020-12-21 17:25:00 +0200

    Name user

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -174,7 +174,7 @@
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 <img class="rounded-circle header-profile-user" src="assets/images/users/avatar-2.jpg"
                                     alt="Header Avatar">
-                                <span class="d-none d-xl-inline-block ml-1">Kevin</span>
+                                <span class="d-none d-xl-inline-block ml-1">{{auth()->user()->name}}</span>
                                 <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
                             </button>
                             <div class="dropdown-menu dropdown-menu-right">
